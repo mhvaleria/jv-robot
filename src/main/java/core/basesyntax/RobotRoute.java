adccc3631082c07dcb2d04aa0d1c584efb7a9846
Valeria <valeriaua97@gmail.com>
2025-05-29 21:07:26 +0200

    Done

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,37 +2,37 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-        int distanceToX = toX - robot.getX();
-        int distanceToY = toY - robot.getY();
+        int distanceToXway = toX - robot.getX();
+        int distanceToYway = toY - robot.getY();
 
-        if (distanceToX > 0) {
+        if (distanceToXway > 0) {
             while (robot.getDirection() != Direction.RIGHT) {
                 robot.turnRight();
             }
-            for (int i = 0; i < distanceToX; i++) {
+            for (int i = 0; i < distanceToXway; i++) {
                 robot.stepForward();
             }
-        } else if (distanceToX < 0) {
+        } else if (distanceToXway < 0) {
             while (robot.getDirection() != Direction.LEFT) {
                 robot.turnRight();
             }
-            for (int i = 0; i < -distanceToX; i++) {
+            for (int i = 0; i < -distanceToXway; i++) {
                 robot.stepForward();
             }
         }
 
-        if (distanceToY > 0) {
+        if (distanceToYway > 0) {
             while (robot.getDirection() != Direction.UP) {
                 robot.turnRight();
             }
-            for (int i = 0; i < distanceToY; i++) {
+            for (int i = 0; i < distanceToYway; i++) {
                 robot.stepForward();
             }
-        } else if (distanceToY < 0) {
+        } else if (distanceToYway < 0) {
             while (robot.getDirection() != Direction.DOWN) {
                 robot.turnRight();
             }
-            for (int i = 0; i < - distanceToY; i++) {
+            for (int i = 0; i < - distanceToYway; i++) {
                 robot.stepForward();
             }
         }
